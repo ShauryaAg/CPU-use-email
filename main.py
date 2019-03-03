b73d47fda1dc09ae4b04b3d1c61a27cf4f4879eb
Shaurya Agarwal <31778302+ShauryaAg@users.noreply.github.com>
2019-03-03 18:56:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import smtplib, ssl, psutil
+
+senderEmail = "sender@gmail.com" #put in your own email address
+senderPass = input("Enter Password: ") #Enter your password
+receiverEmail = "reciever@example.com" #change this to receiver's email
+
+cpu_use_threshold = 50 #percent after which we want to send the email
+interval = 1000 #interval in seconds
+
+while True:
+    cpu_per_usage = psutil.cpu_percent(interval=interval)
+
+
+    port=465
+    context = ssl.create_default_context()
+
+
+    #Next, log in to the server
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(senderEmail, senderPass)
+        server.connect("smtp.gmail.com",465)
+        server.ehlo()
+
+
+    #Message to be sent
+    msg = "Your CPU usage is more than" + str(cpu_use_threshold)
+
+
+    if (cpu_per_usage > cpu_use_threshold):
+        server.sendmail(senderEmail, receiverEmail, msg)
